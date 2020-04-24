8f03b016720fda03acc924c26365c0e483cf2d9b
sudheer panangipalli <pvnssudheer@msitprogram.net>
2020-04-24 15:38:34 +0530

    review

--- project1/import.py
+++ project1/import.py
@@ -17,14 +17,18 @@ def main():
     f = open("books.csv")
     reader = csv.reader(f)
     counter = 0
-    for isbn, title, author, pub_year in reader:
-        if isbn == "isbn":
-            continue
-        counter += 1
-        book = Book(id=counter, isbn=isbn, title=title, author=author, pub_year=int(pub_year))
-        db.session.add(book)
-        print(f"{counter}) Added {title} with {isbn} written by {author} in {pub_year}.")
+    # for isbn, title, author, pub_year in reader:
+    #     if isbn == "isbn":
+    #         continue
+    #     counter += 1
+    #     book = Book(id=counter, isbn=isbn, title=title, author=author, pub_year=int(pub_year))
+    #     db.session.add(book)
+    #     print(f"{counter}) Added {title} with {isbn} written by {author} in {pub_year}.")
+    rev = revi(id=1,username='sid',isbn='0380795272',rating=4,review='Amazing Book')
+    db.session.add(rev)
+    rev = revi(id=2,username='sudheer',isbn='0380795272',rating=4,review='Amazing Book')
+    db.session.add(rev)
     db.session.commit()
-
+    print('added')
 if __name__ == "__main__":
     main()
--- project1/models.py
+++ project1/models.py
@@ -10,3 +10,11 @@ class Book(db.Model):
     author = db.Column(db.String, nullable=False)
     pub_year = db.Column(db.Integer, nullable=False)
     revs = db.Column(db.String, nullable=True)
+
+class revi(db.Model):
+    __tablename__ = 'revi'
+    id = db.Column(db.Integer, primary_key = True)    
+    username = db.Column(db.String)
+    isbn = db.Column(db.String)
+    rating = db.Column(db.Integer)
+    review = db.Column(db.String)
